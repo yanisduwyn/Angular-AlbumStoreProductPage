40f68946a5887aa74a6b4d0b8cdaae9e95ab9645
Yanis Duwyn <yanis.duwyn@atos.net>
2020-12-13 23:33:53 +0100

    added productservice

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import {  } from 'rxjs/add/operator/map'
+
+@Injectable()
+export class ProductService {
+
+  _albumUrl: "../assets/album.json";
+
+  constructor(private _http: Http) {
+    
+   }
+
+   getAlbum(id: number){
+      return this._http.get(this._albumUrl).map()=>{};
+   };
+}
