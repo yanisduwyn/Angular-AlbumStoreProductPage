79012100518121ca0b11482c76e5aba99472a78e
Yanis Duwyn <yanis.duwyn@atos.net>
2020-12-14 00:09:24 +0100

    use single quoites

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,7 +4,7 @@ import {  } from 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
-  private _albumUrl: "../assets/album.json";
+  private _albumUrl: '../assets/album.json';
 
   constructor(private _http: Http) {
     
